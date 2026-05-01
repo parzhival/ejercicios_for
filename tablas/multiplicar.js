1315097cc19a631d6d038d32ca748fadbe331462
parzhival <jean08070225@gmail.com>
2026-05-01 09:59:45 -0500

    feat: New tables are added

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -2,7 +2,7 @@ function generarTablas(){
     let contenido = "";
     let contenedor = document.getElementById("tablas");
     let numero = parseInt(document.getElementById("txtNumb").value);
-    for (let i = 1; i<=10; i++){
+    for (let i = 1; i<=12; i++){
         contenido = contenido + "<div class='fila'>"+ numero +" x " + i +" = "+ (numero * i) + "</div>";
     }
     contenedor.innerHTML = contenido;
